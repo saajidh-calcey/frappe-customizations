6b271b4a7dbbbaa11b1ab9498b3b49d8b92b9f93
saajidh-calcey <saajidh@calcey.com>
2025-12-08 13:23:08 +0000

    feat: Copy Initial CRM files

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <FrappeUIProvider>
+    <NotPermitted v-if="$route.name === 'Not Permitted'" />
+    <Layout class="isolate" v-else-if="session().isLoggedIn">
+      <router-view :key="$route.fullPath" />
+    </Layout>
+    <Dialogs />
+  </FrappeUIProvider>
+</template>
+
+<script setup>
+import NotPermitted from '@/pages/NotPermitted.vue'
+import { Dialogs } from '@/utils/dialogs'
+import { sessionStore as session } from '@/stores/session'
+import { FrappeUIProvider, setConfig } from 'frappe-ui'
+import { computed, defineAsyncComponent } from 'vue'
+
+const MobileLayout = defineAsyncComponent(
+  () => import('./components/Layouts/MobileLayout.vue'),
+)
+const DesktopLayout = defineAsyncComponent(
+  () => import('./components/Layouts/DesktopLayout.vue'),
+)
+const Layout = computed(() => {
+  if (window.innerWidth < 640) {
+    return MobileLayout
+  } else {
+    return DesktopLayout
+  }
+})
+
+setConfig('systemTimezone', window.timezone?.system || null)
+setConfig('localTimezone', window.timezone?.user || null)
+</script>
